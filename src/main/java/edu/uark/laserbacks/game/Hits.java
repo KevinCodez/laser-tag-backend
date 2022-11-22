b3b6bb7fd355f470ba48c1ab0aa9ec96709a033c
Bailey Grimes <baileygrimes@Baileys-MacBook-Pro.local>
2022-11-22 13:18:37 -0600

    Changed Hits to no longer persist

--- src/main/java/edu/uark/laserbacks/game/Hits.java
+++ src/main/java/edu/uark/laserbacks/game/Hits.java
@@ -3,18 +3,10 @@ package edu.uark.laserbacks.game;
 import edu.uark.laserbacks.player.Player;
 import lombok.Data;
 
-import javax.persistence.*;
-
 @Data
-@Entity
 public class Hits {
-    @Id
-    @GeneratedValue
     private Integer id;
-    @OneToOne
     private Team team;
-    @ManyToOne
     private Player shooter;
-    @ManyToOne
     private Player tagged;
 }
