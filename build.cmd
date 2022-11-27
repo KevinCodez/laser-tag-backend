a688f6e9f246809d01167535a710afe9b4a34263
baileyjg <baileyjgrimes@gmail.com>
2022-11-27 13:37:09 -0600

    Windows build file

---
+++ build.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+echo This script will install dependencies for the project such as: node version manager, node.js, angular-cli, and maven.
+set /p userInput="Would you still like to continue? (Y/N): "
+
+set input=false
+if %userInput%==n set input=true
+if %userInput%==N set input=true
+if %input%==true (
+    echo You must download the dependencies manually to run the project.
+    exit 1
+)
+
+git submodule update --init --recursive --remote
+@REM cd frontend
